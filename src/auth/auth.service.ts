c029ee30f4e0bd5ac2ef2b8b27323e27879f6a8a
Simon Nielsen <simon.nielsen@devies.se>
2020-12-26 13:52:24 +0100

    Add admin approved functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,7 +23,11 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user._id };
+    const payload = {
+      username: user.username,
+      sub: user._id,
+      approved: user.approved,
+    };
     return {
       access_token: this.jwtService.sign(payload),
     };
@@ -31,7 +35,11 @@ export class AuthService {
 
   async register(credentials: RegisterDTO): Promise<any> {
     const user = await this.usersService.create(credentials);
-    const payload = { username: user.username, sub: user._id };
+    const payload = {
+      username: user.username,
+      sub: user._id,
+      approved: user.approved,
+    };
     return {
       access_token: this.jwtService.sign(payload),
     };
--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,4 +1,8 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from './public';
@@ -18,5 +22,12 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       return true;
     }
     return super.canActivate(context);
+  }
+
+  handleRequest(err, user) {
+    if (err || !user || !user.approved) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
   }
 }
