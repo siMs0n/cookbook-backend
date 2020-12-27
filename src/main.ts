5380786f8c3aa6e7ddc83721e817fabc52249312
Simon Nielsen <simon.c.nielsen@gmail.com>
2020-12-27 14:23:49 +0100

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
